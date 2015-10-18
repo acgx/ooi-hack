b5358a977f66de1436e96bdb8770cd6236160809
Kitakami Ooi <kitakami@ooi.moe>
2015-10-18 11:08:45 +0800

    已登录用户不显示index视图，而是跳转到home视图

--- oh_pages/views.py
+++ oh_pages/views.py
@@ -1,5 +1,7 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import View, TemplateView
 from django.template.response import TemplateResponse
+from django.shortcuts import redirect
 
 from oh_users.mixins import LoginRequiredMixin
 
@@ -7,7 +9,7 @@ from oh_users.mixins import LoginRequiredMixin
 class Index(View):
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated():
-            pass
+            return redirect(reverse_lazy('home'))
         else:
             return TemplateResponse(self.request, 'oh_pages/index.html')
 

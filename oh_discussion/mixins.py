14a6dcb9c7988d8d575b1233a431a4e4b6b53994
Kitakami Ooi <kitakami@ooi.moe>
2015-10-22 09:38:13 +0800

    Typo

--- oh_discussion/mixins.py
+++ oh_discussion/mixins.py
@@ -10,7 +10,7 @@ class ODiscussionMixin(LoginRequiredMixin):
         return context
 
 
-class SimditorMixin():
+class SimditorMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['extra_css'] = ('css/simditor.css', )
